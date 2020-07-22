493722b8e133bff8f967fb5d26cb0cfa8785aea4
lzyun001 <63327467+lzyun001@users.noreply.github.com>
2020-07-22 14:02:17 +0800

    11

---
+++ Petswar/Assets/Prop.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prop : MonoBehaviour
+{
+    [Header("傷害")]
+    public float damage;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+
+        }
+    }
+}
