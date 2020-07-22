7f0b5aa8d4e007ed6b3d915b5d2c4725d5881478
lzyun001 <63327467+lzyun001@users.noreply.github.com>
2020-07-22 10:05:31 +0800

    throw

---
+++ Petswar/Assets/Player.cs
@@ -0,0 +1,31 @@
+﻿
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject player2;
+    [Header("丟擲速度")]
+    public float speed;
+    [Header("丟擲物品")]
+    public GameObject prop;
+    private void Start()
+    {
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        { 
+            Fire();
+        }
+    }
+    public void Fire()
+    {
+        GameObject temp = Instantiate(prop,transform.position,Quaternion.identity);
+        Vector3 vec = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10));
+        temp.transform.LookAt(vec);
+        temp.GetComponent<Rigidbody>().AddForce(temp.transform.forward*1000);
+        //temp.transform.position = Vector3.MoveTowards(temp.transform.position, player2.transform.position, speed);
+        Destroy(temp, 5f);
+
+    }
+}
